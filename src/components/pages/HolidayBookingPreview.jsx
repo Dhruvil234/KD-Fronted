000dadc5378943c47422d77f4b73c843f37f7aa3
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-01-25 15:01:21 +0530

    Set api calling in package

--- src/components/pages/HolidayBookingPreview.jsx
+++ src/components/pages/HolidayBookingPreview.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate,useLocation } from 'react-router-dom'
 import { toast } from 'react-toastify';
 
@@ -6,7 +6,25 @@ export const HolidayBookingPreview = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const selectedDate = location.state?.selectedDate || null;
-
+    const holidayPackage = location.state?.holidayPackage || null;
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    
+    useEffect(() => {
+      // Check if user details are available in local storage
+      const storedUserName = localStorage.getItem('userFullName');
+      const storedUserEmail = localStorage.getItem('userEmail');
+  
+      // If not available, redirect to register page
+      if (!storedUserName || !storedUserEmail) {
+        navigate('/register');
+      } else {
+        // Set user details for display
+        setUserName(storedUserName);
+        setUserEmail(storedUserEmail);
+      }
+    }, [navigate]);
+    
     const handlebookholiday = () => {
         navigate('/')
         toast.success("Package Booked Sucssesfully");
@@ -20,15 +38,15 @@ export const HolidayBookingPreview = () => {
   return (
     <div className='holidaybookingpreview' style={containerStyle}>
         <h2 className='holidaybookingtag'>Holiday Booking Preview</h2>
-          <h2 className='holidaybookinginfo1'>Red Fort</h2>
-          <p className='holidaybookinginfo2'>Duration: 4 Night/5 Days</p>
-          <p className='holidaybookinginfo3'>Date of Travel: {selectedDate ? selectedDate.toLocaleDateString('en-GB') : 'No date selected'}</p>
-          <p className='holidaybookinginfo4'>Seller : KD Travels</p>
-          <p className='holidaybookinginfo5'>Service : Meals,Hotels,Transfer</p>
-          <p className='holidaybookinginfo6'>Price : 19,999/</p>
-          <p className='holidaybookinginfo7'>Name: Mistry Dhruvil </p>
-          <p className='holidaybookinginfo8'>Email: 21bcubs@gmail.com</p>
-          <button type='submit'onClick={handlebookholiday} className='holidaybookbtn' >Book Now</button>
+        <h2 className='holidaybookinginfo1'>{holidayPackage?.holidayName}</h2>
+        <p className='holidaybookinginfo2'>Duration: {holidayPackage?.duration}</p>
+        <p className='holidaybookinginfo3'>Date of Travel: {selectedDate ? selectedDate.toLocaleDateString('en-GB') : 'No date selected'}</p>
+        <p className='holidaybookinginfo4'>Seller: {holidayPackage?.seller}</p>
+        <p className='holidaybookinginfo5'>Service: {holidayPackage?.service}</p>
+        <p className='holidaybookinginfo6'>Price: {holidayPackage?.price}</p>
+        <p className='holidaybookinginfo7'>Name: {userName}</p>
+        <p className='holidaybookinginfo8'>Email: {userEmail}</p>
+        <button type='submit'onClick={handlebookholiday} className='holidaybookbtn' >Book Now</button>
     </div>
   )
 }
