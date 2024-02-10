61619ad3256370ca365cfc41f610f090fe078e3b
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-02-10 13:42:53 +0530

    Integrate Razorpay api

--- src/components/pages/FlightBookingPreview.jsx
+++ src/components/pages/FlightBookingPreview.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+const RAZORPAY_ID_KEY = import.meta.env.VITE_RAZORPAY_ID_KEY;
+const RAZORPAY_SECRET_KEY=import.meta.env.VITE_RAZORPAY_SECRET_KEY;
+const API = import.meta.env.VITE_BACKENDAPI;
+const sendflightticketapi = `${API}/api/bookflight`;
+
 export const FlightBookingPreview = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -29,9 +34,82 @@ export const FlightBookingPreview = () => {
     }
   }, [navigate]);
 
-  const handleBookNowClick = () => {
+  const sendTicket = async (response) => {
+    // Extracting data from the response and other details
+    const ticketData = {
+      from: departureCity?.label,
+      to: destinationCity?.label,
+      flightClass: classSelection?.label,
+      date: departureDate?.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }),
+      seat: selectedSeat?.label,
+      price: flightprice,
+      name: userName,
+      email: userEmail
+    };
+  
+    try {
+      // Send POST request to the API endpoint
+      const response = await fetch(sendflightticketapi, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ticketData)
+      });
+  
+      // Check if the response is OK
+      if (response.ok) {
+        // Display success message
+        alert('Flight Ticket sended to Your Email.');
+      } else {
+        // Display error message
+        alert('Failed to send flight ticket.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Display error message
+      alert('An error occurred while booking flight ticket');
+    }
+  };
+  
+
+  //  Paymet sucess function
+  const handlePaymentSuccess = (response) => {
+    // Call sendTicket function with the payment response
+    sendTicket(response);
+    // Display success message
+    toast.success('Flight Ticket Booked Successfully');
+    // Navigate to home page
     navigate('/');
-    toast.success("Flight Ticket Booked Sucssesfully");
+  };
+
+
+
+  const handleBookNowClick = () => {
+    const storedUserName = localStorage.getItem('userFullName');
+    const storedUserEmail = localStorage.getItem('userEmail');
+    const options = {
+      key: RAZORPAY_ID_KEY,
+      amount: flightprice * 100, // Amount in smallest currency unit (cents)
+      currency: 'INR',
+      name: 'skynet',
+      description: 'Flight Booking',
+      image: 'https://iili.io/J19QQjI.png', // skynet company logo
+      handler: handlePaymentSuccess,
+      prefill: {
+        name: storedUserName,
+        email: storedUserEmail
+      },
+      notes: {
+        // Additional notes if needed
+      },
+      theme: {
+        color: '#1980c3' // Your theme color
+      }
+    };
+
+    const razorpay = new Razorpay(options);
+    razorpay.open();
   };
 
   const containerStyle = {
--- src/components/pages/HolidayBookingPreview.jsx
+++ src/components/pages/HolidayBookingPreview.jsx
@@ -1,58 +1,143 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate,useLocation } from 'react-router-dom'
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const RAZORPAY_ID_KEY = import.meta.env.VITE_RAZORPAY_ID_KEY;
+const API = import.meta.env.VITE_BACKENDAPI;
+const sendHolidayPackageApi = `${API}/api/bookholiday`;
+
 
 export const HolidayBookingPreview = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const selectedDate = location.state?.selectedDate || null;
-    const holidayPackage = location.state?.holidayPackage || null;
-    const [userName, setUserName] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-    
-    useEffect(() => {
-      // Check if user details are available in local storage
-      const storedUserName = localStorage.getItem('userFullName');
-      const storedUserEmail = localStorage.getItem('userEmail');
-      const token = localStorage.getItem('token');
-  
-      // If not available, redirect to register page
-      if (!storedUserName || !storedUserEmail) {
-        navigate('/register');
-      } else {
-        // Set user details for display
-        setUserName(storedUserName);
-        setUserEmail(storedUserEmail);
-        if(!token){
-          navigate('/Login');
-        }
+  const navigate = useNavigate();
+  const location = useLocation();
+  const selectedDate = location.state?.selectedDate || null;
+  const holidayPackage = location.state?.holidayPackage || null;
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
+  useEffect(() => {
+    // Check if user details are available in local storage
+    const storedUserName = localStorage.getItem("userFullName");
+    const storedUserEmail = localStorage.getItem("userEmail");
+    const token = localStorage.getItem("token");
+
+    // If not available, redirect to register page
+    if (!storedUserName || !storedUserEmail) {
+      navigate("/register");
+    } else {
+      // Set user details for display
+      setUserName(storedUserName);
+      setUserEmail(storedUserEmail);
+      if (!token) {
+        navigate("/Login");
       }
-    }, [navigate]);
-    
-    const handlebookholiday = () => {
-        navigate('/')
-        toast.success("Package Booked Sucssesfully");
     }
-    
-    const containerStyle = {
-        backgroundImage: 'url("https://holidaypackages.yatra.com/media/test_module_1/2021/Aug/72d31d54a22713735e46135fcec834a6.jpg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 1.5)',
-      };
+  }, [navigate]);
+  const sendPackage = async (response) => {
+    // Extracting data from the response and other details
+    const packageData = {
+        holidayTitle: holidayPackage?.holidayName,
+        city: holidayPackage?.city,
+        duration: holidayPackage?.duration,
+        dateOfTravel: selectedDate ? selectedDate.toLocaleDateString('en-GB') : 'No date selected',
+        service: holidayPackage?.service,
+        price: holidayPackage?.price,
+        name: userName,
+        email: userEmail,
+        paymentResponse: response
+    };
+
+    try {
+        // Send POST request to the API endpoint
+        const response = await fetch(sendHolidayPackageApi, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(packageData)
+        });
+
+        // Check if the response is OK
+        if (response.ok) {
+            alert('Holiday Package Detail sended to Your Email');
+        } else {
+            // Display error message
+            alert('Failed to send holiday package to your Email!');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        // Display error message
+        alert('An error occurred while booking holiday package');
+    }
+};
+
+  //  Payment success function
+  const handlePaymentSuccess = (response) => {
+    // Call sendPackage function with the payment response
+    sendPackage(response);
+    // Display success message
+    toast.success("Holiday Package Booked Successfully");
+    // Navigate to home page
+    navigate("/");
+  };
+
+  const handlebookholiday = () => {
+    const storedUserName = localStorage.getItem("userFullName");
+    const storedUserEmail = localStorage.getItem("userEmail");
+    const options = {
+      key: RAZORPAY_ID_KEY,
+      amount: holidayPackage?.price * 100, // Amount in smallest currency unit (cents)
+      currency: "INR",
+      name: "skynet",
+      description: "Holiday Package Booking",
+      image: 'https://iili.io/J19QQjI.png',
+      handler: handlePaymentSuccess,
+      prefill: {
+        name: storedUserName,
+        email: storedUserEmail,
+      },
+      theme: {
+        color: "#0000ff", // Your theme color
+      },
+    };
+
+    const razorpay = new Razorpay(options);
+    razorpay.open();
+  };
+
+  const containerStyle = {
+    backgroundImage:
+      'url("https://holidaypackages.yatra.com/media/test_module_1/2021/Aug/72d31d54a22713735e46135fcec834a6.jpg")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 1.5)",
+  };
   return (
-    <div className='holidaybookingpreview' style={containerStyle}>
-        <h2 className='holidaybookingtag'>Holiday Booking Preview</h2>
-        <h2 className='holidaybookinginfo1'>{holidayPackage?.holidayName}</h2>
-        <p className='holidaybookinginfo2'>City: {holidayPackage?.city}</p>
-        <p className='holidaybookinginfo2'>Duration: {holidayPackage?.duration}</p>
-        <p className='holidaybookinginfo3'>Date of Travel: {selectedDate ? selectedDate.toLocaleDateString('en-GB') : 'No date selected'}</p>
-        <p className='holidaybookinginfo4'>Seller: {holidayPackage?.seller}</p>
-        <p className='holidaybookinginfo5'>Service: {holidayPackage?.service}</p>
-        <p className='holidaybookinginfo6'>Price:Rs. {holidayPackage?.price}/-</p>
-        <p className='holidaybookinginfo7'>Name: {userName}</p>
-        <p className='holidaybookinginfo8'>Email: {userEmail}</p>
-        <button type='submit'onClick={handlebookholiday} className='holidaybookbtn' >Book Now</button>
+    <div className="holidaybookingpreview" style={containerStyle}>
+      <h2 className="holidaybookingtag">Holiday Booking Preview</h2>
+      <h2 className="holidaybookinginfo1">{holidayPackage?.holidayName}</h2>
+      <p className="holidaybookinginfo2">City: {holidayPackage?.city}</p>
+      <p className="holidaybookinginfo2">
+        Duration: {holidayPackage?.duration}
+      </p>
+      <p className="holidaybookinginfo3">
+        Date of Travel:{" "}
+        {selectedDate
+          ? selectedDate.toLocaleDateString("en-GB")
+          : "No date selected"}
+      </p>
+      <p className="holidaybookinginfo4">Seller: {holidayPackage?.seller}</p>
+      <p className="holidaybookinginfo5">Service: {holidayPackage?.service}</p>
+      <p className="holidaybookinginfo6">Price:Rs. {holidayPackage?.price}/-</p>
+      <p className="holidaybookinginfo7">Name: {userName}</p>
+      <p className="holidaybookinginfo8">Email: {userEmail}</p>
+      <button
+        type="submit"
+        onClick={handlebookholiday}
+        className="holidaybookbtn"
+      >
+        Book Now
+      </button>
     </div>
-  )
-}
+  );
+};
